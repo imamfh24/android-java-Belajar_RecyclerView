dce51b394a467967d721bbde9c1bfb2a1510195b
IFA <imamfh24@gmail.com>
2020-04-11 03:13:34 +0700

    Belajar Reycycle View

---
+++ app/src/main/java/com/ifa/myrecyclerview/MainActivity.java
@@ -0,0 +1,106 @@
+package com.ifa.myrecyclerview;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView rvHeroes;
+    private ArrayList<Hero> list = new ArrayList<>();
+    private String title = "Mode List";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setActionBar(title);
+
+
+        rvHeroes = findViewById(R.id.rv_heroes);
+        rvHeroes.setHasFixedSize(true);
+
+        list.addAll(HeroesData.getListData());
+        showRecyclerList();
+    }
+
+    private void showRecyclerList() {
+        rvHeroes.setLayoutManager(new LinearLayoutManager(this));
+        ListHeroAdapter listHeroAdapter = new ListHeroAdapter(list);
+        rvHeroes.setAdapter(listHeroAdapter);
+
+        listHeroAdapter.setOnItemClickCallback(new ListHeroAdapter.OnItemClickCallback() {
+            @Override
+            public void onItemClicked(Hero data) {
+                showSelectedHero(data);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        setMode(item.getItemId());
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setMode(int itemId) {
+        switch (itemId){
+            case R.id.action_list:
+                title = "Mode List";
+                showRecyclerList();
+                break;
+            case R.id.action_grid:
+                title = "Mode Grid";
+                showRecylerGrid();
+                break;
+            case R.id.action_cardview:
+                title = "Mode CardView";
+                showRecyclerCard();
+                break;
+        }
+        setActionBar(title);
+    }
+
+    private void showRecylerGrid(){
+        rvHeroes.setLayoutManager(new GridLayoutManager(this,3));
+        GridHeroAdapter gridHeroAdapter = new GridHeroAdapter(list);
+        rvHeroes.setAdapter(gridHeroAdapter);
+
+        gridHeroAdapter.setOnItemClickCallback(new GridHeroAdapter.OnItemClickCallback() {
+            @Override
+            public void onItemClicked(Hero data) {
+                showSelectedHero(data);
+            }
+        });
+    }
+
+    private void showRecyclerCard(){
+        rvHeroes.setLayoutManager(new LinearLayoutManager(this));
+        CardViewHeroAdapter cardViewHeroAdapter = new CardViewHeroAdapter(list);
+        rvHeroes.setAdapter(cardViewHeroAdapter);
+    }
+
+    private void setActionBar(String title){
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setTitle(title);
+        }
+    }
+
+    private void showSelectedHero(Hero hero){
+        Toast.makeText(this, "Kamu memilih " + hero.getName(), Toast.LENGTH_SHORT).show();
+    }
+}
